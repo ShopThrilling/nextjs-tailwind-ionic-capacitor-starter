7edb373dc56b14964f7f90214f98d97044f6f442
Justin Jackson <jaymjax@gmail.com>
2022-07-05 14:21:53 -0400

    Read only checkout screen

--- components/pages/Cart-Checkout/Cart.jsx
+++ components/pages/Cart-Checkout/Cart.jsx
@@ -21,10 +21,12 @@ import {
 	IonIcon,
 	IonText,
 	IonModal,
-	IonButtons
+	IonButtons,
+	IonInput,
+	IonItemDivider,
 } from '@ionic/react'
 
-import { closeOutline, heartOutline, logoApple, cardOutline } from 'ionicons/icons';
+import { closeOutline, heartOutline, logoApple, cardOutline, homeOutline } from 'ionicons/icons';
 import imgixUtil from '../../../util/imgixUtil';
 
 
@@ -76,7 +78,7 @@ const Cart = () => {
 	const [shoppingBag, setShoppingBag] = useState(products);
 	const [checkoutModal, showCheckoutModal] = useState(false);
 	return (
-		<IonPage>
+		<IonPage className='bg-black'>
 			<IonHeader>
 				<IonToolbar>
 					<IonTitle>
@@ -86,7 +88,7 @@ const Cart = () => {
 			</IonHeader>
 			<section className='flex bg-white py-2 border-t border-b w-screen justify-center'>
 				<p className='flex text-xs font-light'>
-					2 Items: Total (excluding delivery + sales tax) <span className='font-semibold px-1'>$ 46.00</span>
+					3 Items: Total (excluding delivery + sales tax) <span className='font-semibold px-1'>$ 213.99</span>
 				</p>
 			</section>
 
@@ -142,9 +144,9 @@ const Cart = () => {
 						))
 					}
 				</IonList>
-				
+
 				{/** Start of the checkout modal */}
-				<IonModal isOpen={checkoutModal}>
+				<IonModal isOpen={checkoutModal} className='h-full'>
 					<IonHeader>
 						<IonToolbar>
 							<IonButtons slot="primary">
@@ -157,19 +159,124 @@ const Cart = () => {
 							</IonTitle>
 						</IonToolbar>
 					</IonHeader>
-					<section>
-						Delivery Address
-					</section>
-					<section>
-						Delivery Options
-					</section>
-					<section>
-						Payment
-					</section>
+					<IonContent className='my-1'>
+						<div className='bg-gray-100'>
+							<section className='flex-col bg-white py-2 px-3 border-b w-screen my-3'>
+								<p className='text-2xl py-4 font-semibold'>Delivery Address</p>
+								<div className='content-center'>
+									<span>
+										<IonIcon icon={homeOutline} size="small" className='pr-1' />
+										<span className='font-semibold text-lg'>Postal Address</span>
+									</span>
+								</div>
+								<div>
+									<p className='w-3/4 font-light'>
+										Justin Jackson
+										<br />
+										123 Thrilling Ave
+										<br />
+										Washington, DC
+										<br />
+										20002
+										<br />
+										United States
+										<br />
+										(123) 456-7890
+									</p>
+								</div>
+							</section>
+
+							<section className='flex-col bg-white py-2 px-3 border-b border-t w-screen my-3'>
+								<p className='text-2xl py-4 font-semibold'>Delivery Options</p>
+								<div className='mb-5'>
+									<input id="standard" type="radio" />
+									<label htmlFor="standard">
+										<span><span className='font-semibold ml-3'>$4.99</span> Standard Shipping</span>
+									</label>
+									<p className='ml-7 font-light'>
+										Delivered on or before Monday, 10th
+									</p>
+								</div>
+								<div className='mb-5'>
+									<input id="express" type="radio" />
+									<label htmlFor="express">
+										<span><span className='font-semibold ml-3'>FREE</span> Express Delivery</span>
+									</label>
+									<p className='ml-7'>
+										Delivered on or before Monday, 10th
+									</p>
+								</div>
+								<div className='mb-5'>
+									<input id="nextday" type="radio" />
+									<label htmlFor="nextday">
+										<span><span className='font-semibold ml-3'>$7.99</span> Next-Day Delivery</span>
+									</label>
+									<p className='ml-7'>
+										Delivered on or before Monday, 10th
+									</p>
+								</div>
+							</section>
+
+							<section className='flex-col bg-white py-2 px-3 border-b border-t w-screen my-3'>
+								<p className='text-2xl py-4 font-semibold'>Payment</p>
+								<IonList>
+									<IonItem>
+										<IonLabel position='stacked'> Card Number </IonLabel>
+										<IonInput> <IonIcon icon={cardOutline} />4147123456789123</IonInput>
+									</IonItem>
+									<IonItem>
+										<IonLabel position='stacked'> Expiry Date </IonLabel>
+										<IonInput>01/23</IonInput>
+									</IonItem>
+									<IonItem>
+										<IonLabel position='stacked'> Name on Card </IonLabel>
+										<IonInput>Justin Jackson</IonInput>
+									</IonItem>
+									<IonItem>
+										<IonLabel position='stacked'> CVV </IonLabel>
+										<IonInput>123</IonInput>
+									</IonItem>
+								</IonList>
+							</section>
+
+							<section className='flex-col pt-2 pb-5 px-3 w-screen mt-5'>
+								<div className='flex justify-between'>
+									<span>Sub-total</span>
+									<span>$ 213.99</span>
+								</div>
+								<div className='flex justify-between'>
+									<span>Delivery</span>
+									<span>FREE</span>
+								</div>
+								<div className='flex justify-between'>
+									<span>Sales tax</span>
+									<span>$ 15.16</span>
+								</div>
+								<div className='flex justify-between'>
+									<span className='font-semibold'>Total to pay</span>
+									<span className='font-semibold'>$ 229.15</span>
+								</div>
+								<div>
+									<IonButton className='w-full font-semibold text-black my-5 h-14 rounded-none' color='success'>
+										<IonText>
+											Place Order
+										</IonText>
+									</IonButton>
+								</div>
+								<div>
+									<p className='mt-2 font-light text-xs'>
+										By placing your order you agree to our terms and conditions, privacy, and returns policies and consent to some of your data being stored by Thrilling which may be used to make future shopping experiences better for you.
+									</p>
+								</div>
+							</section>
+						</div>
+					</IonContent>
+
 				</IonModal>
 				{/** End of the checkout modal */}
-			
+
 			</IonContent>
+
 			<section className='flex bg-white py-2 divide-x divide-solid border-t border-b'>
 				{['Checkout', 'Apple Pay'].map(v => (
 					<IonButton
